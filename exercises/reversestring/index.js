1b7c84065afef4e0664098b276b567de46c68314
Keyur Patel <ekeyur@gmail.com>
2019-03-04 19:14:35 -0500

    completed string reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,11 +1,15 @@
-// --- Directions
-// Given a string, return a new string with the reversed
-// order of characters
-// --- Examples
-//   reverse('apple') === 'leppa'
-//   reverse('hello') === 'olleh'
-//   reverse('Greetings!') === '!sgniteerG'
+// --- Directions Given a string, return a new string with the reversed order of
+// characters --- Examples   reverse('apple') === 'leppa'   reverse('hello')
+// === 'olleh'   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str
+    .split('')
+    .reduce(((reversed, character) => character + reversed), '');
+}
 
 module.exports = reverse;
+
+// return str   .split('')   .reverse()   .join(''); let reversed = '';
+// ///////////////////////////// for (let character of str) {     reversed =
+// character + reversed;   }   return reversed;
